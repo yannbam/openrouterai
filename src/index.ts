0e7094cdf756d12d36e1d123e87faa43e0c44707
Big Boss <heltonteixeira@gmail.com>
2025-02-10 16:45:48 -0300

    chore: bump version to 2.1.0
    Update package manager to pnpm

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ class OpenRouterServer {
     this.server = new Server(
       {
         name: 'openrouter-server',
-        version: '2.0.0',
+        version: '2.1.0',
       },
       {
         capabilities: {
